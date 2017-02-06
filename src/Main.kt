f02f8168479ea9b023f8f7a659d081dde43fef18
David Kamberský <david.kambersky@gmail.com>
2017-02-06 18:54:09 +0000

    added Discord4J; example in main
    woo

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,18 @@
+import sx.blah.discord.api.ClientBuilder
+
+
 /**
  * Created by David on 06/02/2017.
  */
 fun main(args: Array<String>) {
     println("Hello, human")
+
+
+    /* Lib working */
+    val builder = ClientBuilder()
+
+
 }
+
+
+
