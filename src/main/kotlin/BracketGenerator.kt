cc07c1643f920cbc730f13e8d33f193ea055d4d7
dusan.pilka@gmail.com <dusan.pilka@gmail.com>
2017-04-10 21:56:23 +0100

    message

---
+++ src/main/kotlin/BracketGenerator.kt
@@ -0,0 +1,10 @@
+import com.exsoloscript.challonge.model.Tournament
+
+/**
+ * Created by travaros on 10/04/2017.
+ */
+fun getBracket(t: Tournament):String {
+
+
+    return ""
+}
---
+++ src/main/kotlin/Component.kt
@@ -0,0 +1,11 @@
+/**
+ * Created by travaros on 10/04/2017.
+ */
+abstract class Component{
+    abstract val drawer:AsciiDrawer
+
+}
+
+class Endpoint{
+    
+}
